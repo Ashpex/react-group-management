346432ed02e7d33cf612aaffa2b5f4b796a7596a
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2022-12-11 10:10:25 +0700

    create branch update-ui

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  styledComponents: true,
-};
-
-module.exports = nextConfig;
--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-const MENU_TABS = {
-  NEW_FEED: "new-feed",
-  EXCERCISE: "exercise",
-  MEMBER: "member",
-};
-
-export { MENU_TABS };
