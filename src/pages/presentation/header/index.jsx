739e63dfc4abebf2c6b209ea043a902de0da776a
HongQuan <lhquan244@gmail.com>
2023-01-10 21:03:49 +0700

    ++up

--- src/pages/presentation/header/index.jsx
+++ src/pages/presentation/header/index.jsx
@@ -1,10 +1,14 @@
 import { Box, Button } from "@mantine/core";
+import { IconPresentation } from "@tabler/icons";
 import React from "react";
 
 export default function PresentationHeader({ createSlide }) {
   return (
     <Box>
       <Button
+        variant="outline"
+        color="green"
+        leftIcon={<IconPresentation/>}
         sx={{
           width: "140px",
         }}
